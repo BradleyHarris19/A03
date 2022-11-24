b9b416987d4bdcf47f21231c5c03fddd0cf2e3a1
Bradley Harris <bradx135@gmail.com>
2022-11-24 21:20:18 +0000

    Created Test class

---
+++ src/test/java/TestColour.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+/*
+* The Colour class must implement the following functionality
+* • A constructor for a Colour object with three parameters which specify the colour components, in
+*   the order red, green and blue, using a floating point values in the range 0.0 to 1.0
+* • A constructor for a Colour object that specifies one parameter which is a combined RGB value
+*   consisting of the red component in bits 16-23, the green component in bits 8-15, and the blue
+*   component in bits 0-7
+* • It is not allowed to create a Colour object without specifying valid component values
+* • Colours can be compared, to be equal they must have the same component values
+*/
+
+public class TestColour {
+
+    @Test
+    public void ColourFloatTest() {
+
+    }
+
+    @Test
+    public void ColourBitsTest() {
+
+    }
+}
+
